8b5e6f227de8af842184a96bb21316dc80e6ab40
Davi Barreto <davivbarreto@gmail.com>
2021-07-13 14:17:06 +0300

    Add module schematic

--- src/lib/resource/resource.factory.test.ts
+++ src/lib/resource/resource.factory.test.ts
@@ -116,10 +116,11 @@ export class UserService extends PrismaCrudService {
         .toEqual(`import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, PrismaService]
 })
 export class UserModule {}
 `);
